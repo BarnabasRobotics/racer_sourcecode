abbe83da8e8b5f07db8ac3acf319243d36fc0483
ericrlin <eric@barnabasrobotics.com>
2018-11-10 13:05:41 -0800

    Add files via upload

---
+++ ultrasonic.ino
@@ -0,0 +1,33 @@
+int ultrasonic(int trigPin, int echoPin)
+{
+  long duration;
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  duration = duration / 59;
+  if ((duration < 2) || (duration > 300)) return false;
+  return duration;
+}
+
+
+void setup()
+{
+  pinMode( 7 , OUTPUT);
+  digitalWrite( 3 , LOW );
+
+}
+
+void loop()
+{
+  digitalWrite( 7 , HIGH );
+  delay( 	ultrasonic( 3 , 4 ) );
+  digitalWrite( 7 , LOW );
+  delay( 	ultrasonic( 3 , 4 ) );
+}
+
+
