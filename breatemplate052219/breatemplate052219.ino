3330b1508d17be2ca8b2b0ff03aa30af81be7c62
ericrlin <eric@barnabasrobotics.com>
2019-05-22 08:14:26 -0700

    Add files via upload

---
+++ breatemplate052219/breatemplate052219.ino
@@ -0,0 +1,64 @@
+#include <Servo.h>
+Servo LM;
+Servo RM;
+
+void go(){
+  LM.write(180);
+  RM.write(0);
+}
+
+void halt(){
+  LM.write(90);
+  RM.write(90);
+}
+
+void left(){
+  LM.write(180);
+  RM.write(180);
+}
+
+void right(){
+  LM.write(0);
+  RM.write(0);
+}
+
+void back(){
+  LM.write(0);
+  RM.write(180);
+}
+
+int getdistance(){
+  long duration;
+  digitalWrite(3,LOW);
+  delayMicroseconds(2);
+  digitalWrite(3,HIGH);
+  delayMicroseconds(20);
+  digitalWrite(3,LOW);
+  duration=pulseIn(4,HIGH);
+  duration=duration/59;
+  if ((duration<2) || (duration>300)) return false;
+  return duration;
+}
+
+void setup() {
+  LM.attach(9);
+  RM.attach(10);
+  pinMode(3,OUTPUT);
+  pinMode(4,INPUT);
+  pinMode(7,OUTPUT);
+  pinMode(2,INPUT_PULLUP);
+  while (digitalRead(2)==HIGH){
+    
+  }
+}
+
+void loop() {
+  x=getdistance();
+  if(){
+    
+  }
+  else{
+    
+  }
+
+}
